803546bad2302ad79753ae0bccfa9e184ae53db6
NikolayevDmitry <NikolayevDmitry@yandex.ru>
2018-02-10 16:04:37 +0300

    commit

--- sbin/pfctl/pfctl.c
+++ sbin/pfctl/pfctl.c
@@ -2515,8 +2515,11 @@ main(int argc, char *argv[])
 
 	/* SKYNICK XXX */
 	if (state_changers) {
-		printf("state_change[0] = %s", state_change[0]);
-		printf("state_change[1] = %s", state_change[1]);
+		printf("state_change[0] = %s\n", state_change[0]);
+		printf("state_change[1] = %s\n", state_change[1]);
+		printf("dev = %s\n", dev);
+		printf("ifcaceopt = %s\n", ifaceopt);
+		printf("opts = %s\n", opts);
 		if (!strcmp(state_change[0], "label"))
 			pfctl_label_change_states(dev, ifaceopt, opts);
 		else if (!strcmp(state_change[0], "id"))
