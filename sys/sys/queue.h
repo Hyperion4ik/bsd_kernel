bd7de904a1327b5b06bcdaf6007af67a5ae6f0bc
Hyperion4ik <Hyperion4ik@gmail.com>
2018-02-18 17:30:05 +0300

    auto

--- sys/sys/queue.h
+++ sys/sys/queue.h
@@ -564,8 +564,6 @@ struct {								\
 		LIST_NEXT((elm), field)->field.le_prev = 		\
 		    (elm)->field.le_prev;				\
 	*(elm)->field.le_prev = LIST_NEXT((elm), field);		\
-//	TRASHIT(*oldnext);						\
-//	TRASHIT(*oldprev);						\
 } while (0)
 /* SKYNICK */
 
